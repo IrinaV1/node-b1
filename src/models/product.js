54ab309a49f8bdb32ea90819f565c87bdcc6ccb5
Irina <vyustirina@gmail.com>
2026-08-24 22:22:16 +0300

    add .

--- src/models/product.js
+++ src/models/product.js
@@ -1,13 +1,32 @@
-import { model, Schema } from "mongoose";
+import { model, Schema } from 'mongoose';
 
-const productSchema = new Schema({
-    name: { type: String, required: true },
-    price: { type: Number, required: true },
-    category: { type: String, enum: ['books', 'electronics', 'clothing', 'other'], required: true, default: 'other' },
-    description: { type: String}
-}, {
-    timestamps: true
-});
-
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
+    category: {
+      type: String,
+      enum: ['books', 'electronics', 'clothing', 'other'],
+      //   required: true,
+      default: 'other',
+    },
+    description: {
+      type: String,
+      default: '',
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
 
 export const Product = model('Product', productSchema);
--- src/server.js
+++ src/server.js
@@ -2,12 +2,12 @@ import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
 
-import {logger} from './middleware/logger.js';
+import { logger } from './middleware/logger.js';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import productsRoutes from './routes/productsRoutes.js';
-
+import helmet from 'helmet';
 
 const app = express();
 const PORT = process.env.PORT ?? 3030;
@@ -15,8 +15,9 @@ const PORT = process.env.PORT ?? 3030;
 app.use(logger);
 app.use(express.json());
 app.use(cors());
+app.use(helmet());
 
-app.use(productsRoutes)
+app.use(productsRoutes);
 
 app.use(errorHandler);
 app.use(notFoundHandler);
